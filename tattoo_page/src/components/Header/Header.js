ad74f965929751ca4b13e637c229ac8fc51c4a79
Khalac <74862194+Khalac@users.noreply.github.com>
2023-07-23 21:37:56 +0700

    delete onClick event that don't do anything

--- tattoo_page/src/components/Header/Header.js
+++ tattoo_page/src/components/Header/Header.js
@@ -1,24 +1,39 @@
-import React from 'react'
-import './Header.scss'
-import {Link} from "react-router-dom"
-import { useState } from 'react'
-import { AiOutlineMenu,AiOutlineClose } from "react-icons/ai"
+import React from "react";
+import "./Header.scss";
+import { Link } from "react-router-dom";
+import { useState } from "react";
+import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
 function Header() {
   const [isMobile, setIsMobile] = useState(false);
   return (
-    <div className={isMobile? 'header_mobile' :'header'} >
-        <Link to='/' className='page_name'>  <img src = {require('./logo.webp')} alt = 'img' className='header_logo' /> </Link>
-        <div className={isMobile? 'left_header_mobile' :'left_header'} onClick= {() => setIsMobile(false)}>
-          <Link to='/About' className='left_header_component'>About</Link>
-          <Link to='/Tattoos' className='left_header_component'>Tattoos</Link>
-          <Link to='/Art' className='left_header_component'>Art</Link>
-          <Link to='/Contact' className='left_header_component'>Contact</Link>
-        </div>
-        <div className='MenuIcon_mobile' onClick={() => setIsMobile(!isMobile)}>
-          {isMobile ? <AiOutlineClose/> : <AiOutlineMenu/>}
-        </div>
+    <div className={isMobile ? "header_mobile" : "header"}>
+      <Link to="/" className="page_name">
+        {" "}
+        <img
+          src={require("./logo.webp")}
+          alt="img"
+          className="header_logo"
+        />{" "}
+      </Link>
+      <div className={isMobile ? "left_header_mobile" : "left_header"}>
+        <Link to="/About" className="left_header_component">
+          About
+        </Link>
+        <Link to="/Tattoos" className="left_header_component">
+          Tattoos
+        </Link>
+        <Link to="/Art" className="left_header_component">
+          Art
+        </Link>
+        <Link to="/Contact" className="left_header_component">
+          Contact
+        </Link>
+      </div>
+      <div className="MenuIcon_mobile" onClick={() => setIsMobile(!isMobile)}>
+        {isMobile ? <AiOutlineClose /> : <AiOutlineMenu />}
+      </div>
     </div>
-  )
+  );
 }
 
-export default Header
+export default Header;
